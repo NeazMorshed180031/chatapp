f5b4686b14bb3d2d2e68667e749d8118e3489edf
neazxeon <neaz@xeon.email>
2024-03-28 02:15:06 +0600

    button visible related issue solved

--- frontend/src/components/messages/MessageInput.jsx
+++ frontend/src/components/messages/MessageInput.jsx
@@ -3,7 +3,7 @@ import { BsSend } from "react-icons/bs";
 const MessageInput = () => {
   return (
     <form className="px-4 my-3">
-      <div className="w-full">
+      <div className="w-full relative">
         <input
           type="text"
           className="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 text-white"
@@ -13,10 +13,36 @@ const MessageInput = () => {
           type="submit"
           className="absolute inset-y-0 end-0 flex items-center pe-3"
         >
-          <BsSend />
+          <BsSend  className="text-white"/>
         </button>
       </div>
     </form>
   );
 };
 export default MessageInput;
+
+
+
+//Starter Code
+// import React from "react";
+// import { BsSend } from "react-icons/bs";
+// const MessageInput = () => {
+//   return (
+//     <form className="px-4 my-3">
+//       <div className="w-full">
+//         <input
+//           type="text"
+//           className="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 text-white"
+//           placeholder="Send a message"
+//         />
+//         <button
+//           type="submit"
+//           className="absolute inset-y-0 end-0 flex items-center pe-3"
+//         >
+//           <BsSend />
+//         </button>
+//       </div>
+//     </form>
+//   );
+// };
+// export default MessageInput;
