ecc342905425d10ad36d215bb808301db82f0750
neazxeon <neaz@xeon.email>
2024-04-11 03:04:47 +0600

    search methodology is implemented

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoSearchSharp } from "react-icons/io5";
+import useConversation from "../../zustand/useConversation";
+import useGetConversations from "../../hooks/useGetConveresation";
+import toast from "react-hot-toast";
 const SearchInput = () => {
+  const [search, setSearch] = useState("");
+  const { setSelectedConversation } = useConversation();
+  const { conversations } = useGetConversations();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!search) return;
+
+    if (search.length < 3) {
+      return toast.error("Search term must be greater tham 4 character");
+    }
+
+    // Overall, this line of code is searching through an array of conversation objects (conversations) to find the first conversation where the fullName property matches (partially or completely, case-insensitive)
+    // the search string. If found, it returns that conversation object; otherwise, it returns undefined.
+
+    const conversation = conversations.find((c) =>
+      c.fullName.toLowerCase().includes(search.toLowerCase())
+    );
+
+    if (conversation) {
+      setSelectedConversation(conversation);
+      setSearch("");
+    } else toast.error("No such user found");
+  };
+
   return (
-    <form className="flex items-center gap-2">
+    <form onSubmit={handleSubmit} className="flex items-center gap-2">
       <input
         type="text"
         placeholder="Type here"
         className="input input-bordered w-full max-w-xs"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
       />
       <button type="submit" className="btn btn-circle bg-sky-500 text-white">
         <IoSearchSharp className="w-6 h-6 outline-none" />
@@ -16,7 +46,6 @@ const SearchInput = () => {
 };
 
 export default SearchInput;
-
 
 // import React from "react";
 // import { IoSearchSharp } from "react-icons/io5";
@@ -36,4 +65,3 @@ export default SearchInput;
 // };
 
 // export default SearchInput;
-
